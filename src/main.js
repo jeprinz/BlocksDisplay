a501b886f366e8a8ff0bb9a0f6ed7420f2c7325d
Jacob Prinz <jacobeliasprinz@gmail.com>
2018-11-05 14:46:15 -0500

    AFter xreactor fix

--- src/main.js
+++ src/main.js
@@ -6,20 +6,23 @@ import {xdiv} from "./X-Html"
 import {makeLeaf, makeBranch} from "./X-Tree"
 import type {XTree} from "./X-Tree"
 import {mirrorHTMLTree} from "./mirrorTree"
+import {List} from 'immutable'
 
 let timesClicked = xvar(() => 0);
 
-let button = only.html({button: "Click!"})
+let button : HTMLElement = only.html({button: "Click!"})
 
 button.addEventListener("click", () => {
   timesClicked.set(() => timesClicked.get() + 1)
 })
 
 let leafy  = xvar(() => only.html({p : "leafy initial text"}))
-let tree : XTree<HTMLElement> = makeBranch(xvar(() =>[
+let branch = xvar(() => List.of(
     makeLeaf(xvar(() => only.html({p : "Leaf1"}))),
     makeLeaf(leafy)
-  ]))
+  ))
+let tree : XTree<HTMLElement> = makeBranch(branch);
+// let display = xdiv(() => branch.xget().map((tree) => tree((t) => t, () => {})).toArray()[0])
 
 only.setHtml([
   {p : "Hello World!"},
@@ -27,3 +30,5 @@ only.setHtml([
   xdiv(() => only.html({p : "Times clicked:" + timesClicked.xget()})),
   mirrorHTMLTree(tree)
   ])
+
+window.branch = branch
--- src/mirrorTree.js
+++ src/mirrorTree.js
@@ -11,12 +11,28 @@ import {List} from "immutable"
 export function mirrorHTMLTree(tree : XTree<HTMLElement>) : HTMLElement{
   return tree(
     /*Leaf*/ (t) => xdiv(() => t.xget()),
-    /*Branch*/ (branch: XVar<List<XTree<HTMLElement>>>) => xdiv(() => only.html({
-        div : branch.xget().map((tree) => mirrorHTMLTree(tree)),
+    /*Branch*/ (branch: XVar<List<XTree<HTMLElement>>>) => xdiv(() => {
+
+      let div : HTMLElement = only.html({
+        div : branch.xget().map((tree) => mirrorHTMLTree(tree)).toArray(),
         css : {
           border : "solid",
           display : "inline-block"
         }
-      }))
+      })
+      return div
+    })
   )
 }
+
+      // let addLeafButton: HTMLElement = only.html({button : "addLeaf"})
+      // addLeafButton.addEventListener("click", () => {
+      //   let newLeaf : XTree<HTMLElement> = makeLeaf(xvar(() => only.html({p : "New leaf"})))
+      //   branch.set(() => branch.get().concat(newLeaf))
+      // })
+      //
+      // let addBranchButton: HTMLElement = only.html({button : "addBranch"})
+      // addBranchButton.addEventListener("click", () => {
+      //   let newBranch : XTree<HTMLElement> = makeBranch(xvar(() => List.of()))
+      //   branch.set(() => branch.get().concat(newBranch))
+      // })
